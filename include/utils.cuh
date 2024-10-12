22a1fadf0f80a76f53cb16e4871f731596bfdbf3
Guangyan Cai <gcai3@uci.edu>
2024-10-11 23:38:44 -0700

    Fixed utils.

--- include/utils.cuh
+++ include/utils.cuh
@@ -3,10 +3,19 @@
 #include <nanobind/nanobind.h>
 
 template<typename T>
-T* device_to_host(const T* d_ptr, size_t size);
+T* device_to_host(const T* d_ptr, size_t size) {
+    T* h_ptr = (T*) malloc(size * sizeof(T));
+    cudaMemcpy(h_ptr, d_ptr, size * sizeof(T), cudaMemcpyDeviceToHost);
+    return h_ptr;
+}
 
 template<typename T>
-T* host_to_device(const T* h_ptr, size_t size);
+T* host_to_device(const T* h_ptr, size_t size){
+    T* d_ptr;
+    cudaMalloc((void **) &d_ptr, size * sizeof(T));   
+    cudaMemcpy(d_ptr, h_ptr, size * sizeof(T), cudaMemcpyHostToDevice);
+    return d_ptr;
+}
 
 nanobind::capsule create_host_capsule(void* ptr);
 
