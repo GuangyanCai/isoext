84ea9a5ed2c19856038c9afaa71bfefd81873b46
Guangyan Cai <gcai3@uci.edu>
2024-10-13 00:02:39 -0700

    Move the lut to a separated file.

---
+++ include/lut.cuh
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace mc {
+
+namespace lorensen {
+
+extern const int edges[];
+extern const int edge_table[];
+extern const int tri_table[];
+
+extern const size_t edges_size, edge_table_size, tri_table_size;
+}  // namespace lorensen
+
+}  // namespace mc
