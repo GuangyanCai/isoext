71224ba6d51952a04c01a928b147c499c4888e55
Guangyan Cai <gcai3@uci.edu>
2024-11-18 03:06:00 -0800

    Updated the mc method to allow specifying cell positions even when the layout is not tight.

--- include/ndarray_types.cuh
+++ include/ndarray_types.cuh
@@ -7,7 +7,7 @@
 using GridType = nanobind::ndarray<nanobind::pytorch, float, nanobind::ndim<3>,
                                    nanobind::device::cuda, nanobind::c_contig>;
 using CellType =
-    nanobind::ndarray<nanobind::pytorch, float, nanobind::shape<-1, 2, 2, 3>,
+    nanobind::ndarray<nanobind::pytorch, float, nanobind::shape<-1, -1, -1, 3>,
                       nanobind::device::cuda, nanobind::c_contig>;
 using AABBType = std::array<float, 6>;
 
