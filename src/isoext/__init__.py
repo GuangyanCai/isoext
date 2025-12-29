80b0860aa56980fa3e2fea9a158922da3abf50d1
GuangyanCai <gc.nick@pm.me>
2025-12-29 11:06:42 -0500

    Added a detection for PyTorch installation.

--- src/isoext/__init__.py
+++ src/isoext/__init__.py
@@ -1,3 +1,7 @@
+import importlib.util
+
+if importlib.util.find_spec("torch") is None:
+    raise ImportError("PyTorch is required but not installed. Please install PyTorch with CUDA support.\n")
+
 from .isoext_ext import *
-from .utils import write_obj, make_grid
-from . import sdf
+from .utils import make_grid, write_obj
